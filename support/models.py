f1def722ea9d306a9dfcfd12d0d966be789ba0ee
Yaroslav <yroslavkumpan@gmail.com>
2026-01-20 20:28:37 +0300

    Add initial support app models

--- support/models.py
+++ support/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ("user", "User"),
+        ("support", "Support"),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    role = models.CharField(
+        choices=ROLE_CHOICES,
+        max_length=20,
+        default="user",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class SupportRequest(models.Model):
+    pass
+
+
+
+class SupportMessage(models.Model):
+    pass
