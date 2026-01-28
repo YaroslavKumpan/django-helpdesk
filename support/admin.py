d37d12d86694623b01a62bb8a43955e1e6a55e2e
Yaroslav <yroslavkumpan@gmail.com>
2026-01-28 23:21:27 +0300

    Added models to admin, improved admin interface

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import SupportRequest, SupportMessage, UserProfile
 
-# Register your models here.
+class SupportRequestAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'created_at', 'user']  # Показать столбцы
+    search_fields = ['title', 'status']  # Добавить поиск
+
+class SupportMessageAdmin(admin.ModelAdmin):
+    list_display = ['support_request', 'sender', 'created_at']  # Показать столбцы
+    search_fields = ['text']  # Добавить поиск по тексту
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role', 'created_at']  # Показать столбцы
+
+
+admin.site.register(SupportRequest, SupportRequestAdmin)
+admin.site.register(SupportMessage, SupportMessageAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- support/models.py
+++ support/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class UserProfile(models.Model):
@@ -20,7 +20,7 @@ class UserProfile(models.Model):
         default="user",
     )
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -29,19 +29,20 @@ class UserProfile(models.Model):
 class SupportRequest(models.Model):
     STATUS_CHOICES = (
         ("new", "New"),
-        ("at_work", "At Work"),
-        ("closed","Сlosed"),
+        ("in_progress", "In Progress"),
+        ("closed","Closed"),
         ("rejected","Rejected")
     )
 
     user = models.ForeignKey(
         UserProfile,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        null=True,
         related_name="support_requests"
     )
 
     title = models.CharField(max_length=255,)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True)
 
     status = models.CharField(
         choices=STATUS_CHOICES,
@@ -55,4 +56,36 @@ class SupportRequest(models.Model):
         return f"{self.title} - {self.get_status_display()}"
 
 class SupportMessage(models.Model):
-    pass
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="support_messages",
+        null=True,
+        blank=True,
+        verbose_name="Отправитель сообщения"
+    )
+
+    support_request = models.ForeignKey(
+        SupportRequest,
+        on_delete=models.CASCADE,
+        related_name="messages",
+        verbose_name="Заявка"
+    )
+
+    text = models.TextField(
+        verbose_name="Текст сообщения",
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        verbose_name="Дата создания"
+    )
+
+    class Meta:
+        verbose_name = "Сообщение в поддержку"
+        verbose_name_plural = "Сообщения в поддержку"
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"Message #{self.id} from {self.sender.username}"
