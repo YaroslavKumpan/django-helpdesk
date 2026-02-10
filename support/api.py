69d91c6dcc938fe86286db0d4a6b778f2171778b
Yaroslav <yroslavkumpan@gmail.com>
2026-02-10 21:14:09 +0300

    created UserRegistrationView and UserLoginView

--- support/api.py
+++ support/api.py
@@ -1,7 +1,22 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 
-class PingView(APIView):
-    def get(self, request):
-        return Response({"ok": True}, status=status.HTTP_200_OK)
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
