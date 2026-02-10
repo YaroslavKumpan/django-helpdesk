34ce84109c9549cac8a0fccee361a8ae46b31b63
Yaroslav <yroslavkumpan@gmail.com>
2026-02-10 21:05:22 +0300

    create UserLoginSerializer

--- support/serializers.py
+++ support/serializers.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -29,3 +31,21 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         )
         return user
 
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials.")
+
+        # генерим JWT токен
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
