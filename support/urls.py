d853cc866206e698682bdd2ff1f3074811d658f9
Yaroslav <yroslavkumpan@gmail.com>
2026-02-24 21:23:53 +0300

    created router and configuring routes for working with requests

--- support/urls.py
+++ support/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .api import UserRegistrationView, UserLoginView, SupportRequestViewSet
 
-from .api import UserRegistrationView, UserLoginView
+router = DefaultRouter()
+router.register(r'requests', SupportRequestViewSet, basename='supportrequest')
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
 ]
+
+urlpatterns += router.urls
