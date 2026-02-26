eaa78bd77abf66e285c4fb9ca6f953b07a72793b
Yaroslav <yroslavkumpan@gmail.com>
2026-02-26 20:00:12 +0300

    used @action inside SupportRequestViewSet, This will create an endpoint that accepts the message text and automatically binds it to the specified request

--- support/api.py
+++ support/api.py
@@ -1,13 +1,14 @@
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions
 
 from .permissions import IsAuthorOrSupport
 from .models import SupportRequest
-from .serializers import UserRegistrationSerializer, UserLoginSerializer, SupportRequestSerializer
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, SupportRequestSerializer, \
+    SupportMessageSerializer
 
 
 class UserRegistrationView(APIView):
@@ -62,3 +63,24 @@ class SupportRequestViewSet(viewsets.ModelViewSet):
         if profile.role != 'admin':
             raise PermissionDenied("Только администратор может удалять заявки.")
         return super().destroy(request, *args, **kwargs)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        serializer_class=SupportMessageSerializer,
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def messages(self, request, pk=None):
+        """
+        Эндпоинт для создания сообщения в заявке.
+        POST /api/requests/{id}/messages/
+        """
+        support_request = self.get_object()
+        data = request.data.copy()
+        data["support_request"] = support_request.id
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
