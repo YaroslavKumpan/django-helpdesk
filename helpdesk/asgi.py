1309d348f37c06692fafd88b7302693265631cd0
Yaroslav <yroslavkumpan@gmail.com>
2026-03-03 20:45:46 +0300

    Installing dependencies (channels, channels-redis, daphne) and configuring ASGI in the project

--- helpdesk/asgi.py
+++ helpdesk/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for helpdesk project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from support import routing  # создадим позже
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpdesk.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),  # обычные HTTP запросы
+    'websocket': AuthMiddlewareStack(  # WebSocket с поддержкой авторизации
+        URLRouter(
+            routing.websocket_urlpatterns  # наши маршруты WebSocket
+        )
+    ),
+})
